5e5ed7674a37f7abbb617c0f30118607a436efb6
JuliaGavrilenko <yulya952@rambler.ru>
2019-04-04 18:41:15 +0200

    run master in background

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -11,11 +11,14 @@ mkdir -p $SPARK_MASTER_LOG
 
 #ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-echo "Copying spark-master service to /etc/init.d"
-cp /etc/simple_grid/config/spark-master /etc/init.d/
-chmod 755 /etc/init.d/spark-master
+#echo "Copying spark-master service to /etc/init.d"
+#cp /etc/simple_grid/config/spark-master /etc/init.d/
+#chmod 755 /etc/init.d/spark-master
 
 echo "Starting Spark Master"
-service spark-master start
-echo "Checking status of Spark Master"
-service spark-master status
+nohup /spark/bin/spark-class org.apache.spark.deploy.master.Master \
+            --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out 2>&1 </dev/null &
+
+#service spark-master start
+#echo "Checking status of Spark Master"
+#service spark-master status
