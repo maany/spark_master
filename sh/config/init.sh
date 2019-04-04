19dd30be73591e4c5bff7a11fd656b31e175005a
JuliaGavrilenko <yulya952@rambler.ru>
2019-04-04 18:27:37 +0200

    add service for spark, make init.sh compatible with SIMPLE

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -11,5 +11,11 @@ mkdir -p $SPARK_MASTER_LOG
 
 #ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
-    --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out 2>&1 &
+echo "Copying spark-master service to /etc/init.d"
+cp /etc/simple_grid/config/spark-master /etc/init.d/
+chmod 755 /etc/init.d/spark-master
+
+echo "Starting Spark Master"
+service spark-master start
+echo "Checking status of Spark Master"
+service spark-master status
--- simple_spark_master.sh
+++ simple_spark_master.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+### CLEANUP ####
+docker stop simple_spark_master && docker rm simple_spark_master
 
 ### BOOT EVENT ###
 sudo docker build -t simple_spark_master sh/
@@ -10,12 +12,12 @@ sudo docker run -itd \
     -p 6066:6066 \
     -v $(pwd)/sh/config:/etc/simple_grid/config \
     -v $(pwd)/augmented_site_level_config_file.yaml:/etc/simple_grid/augmented_site_level_config_file.yaml \
-    simple_spark_master \
-    /bin/bash
+    simple_spark_master
+
 #### PRE INIT HOOKS #####
 
 
 ### INIT EVENT ######
-sudo docker exec -it simple_spark_master /etc/simple_grid/config/init.sh
+#sudo docker exec -it simple_spark_master /etc/simple_grid/config/init.sh
 
 #### POST INIT HOOKS ######
